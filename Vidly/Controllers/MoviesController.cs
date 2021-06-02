311d653144e45832ad52cb26f6a72a1a30478730
Rupal Jain <rupal.jain@nagarro.com>
2021-06-02 16:22:43 +0530

    Navigation Bar

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,10 +10,24 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id=1, Name="Shrek"},
+                new Movie{ Id=2, Name="Wall-e"}
+            };
+        }
+
+        // GET: Movies/Random
         public ActionResult Random()
         {
-
             //instance of movie model
             var movie = new Movie() { Name = "Shrek!" };
             var customers = new List<Customer>
@@ -29,6 +43,7 @@ namespace Vidly.Controllers
             };
 
             return View(viewModel);
+
         }
     }
 }
