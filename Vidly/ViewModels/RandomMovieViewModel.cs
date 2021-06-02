9f5d7b17411282d21fe04484d98bb569c8c23621
Rupal Jain <rupal.jain@nagarro.com>
2021-06-02 16:09:44 +0530

    Add project files.

---
+++ Vidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vidly.Models;
+using System.Web;
+
+namespace Vidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
