9d685cfa5129edd24fa895fdeb607e8b983efc79
Rupal Jain <rupal.jain@nagarro.com>
2021-06-02 17:19:02 +0530

    Add Migration

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using Vidly.Models;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Vidly.Controllers
 {
+
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        //dbcontext to access the database
+        public CustomersController()
+        {
+            //initialise dbcontext in constructor
+            _context = new ApplicationDbContext();
+        }
+
+        //since dbcontext is disposable object, so we dispose by overriding the dispose method of base controller class
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
             return View(customers);
 
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer{ Id =1, Name="John Smith" },
-                new Customer {Id=2, Name ="Mary Williams"}
-            };
-        }
+        
     }
 }
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 using System.Web;
 
 namespace Vidly.Models
@@ -9,7 +10,13 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }          //Navigation property to navigate from one type to another; here
+
+        public byte MembershipTypeId { get; set; }
     }
 }
